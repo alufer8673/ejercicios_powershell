1c4ef633a46af4917fe8f372b3423f92a36eb210
alufer8673 <144796480+alufer8673@users.noreply.github.com>
2023-09-18 16:56:43 +0200

    Add files via upload

---
+++ ejercicio_3.ps1
@@ -0,0 +1,10 @@
+﻿<#
+Ejercicio3. Crea un script en PowerShell que pregunte al usuario por el número de horas
+trabajadas y el coste por hora. Después debe mostrar por pantalla el salario que debemos
+pagarle.
+#>
+
+[int] $horas_trabajadas = Read-Host "Introduce el número de horas trabajadas: "
+[double] $coste_hora = Read-Host "Introduce el coste por hora: "
+$salario = $horas_trabajadas * $coste_hora
+Write-Host "El salario total es de: $salario"
