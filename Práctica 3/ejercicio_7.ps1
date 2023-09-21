f3588becd04928fa298366bc29934edd232de5e9
alufer8673 <alufer8673@ieselcaminas.org>
2023-09-21 09:49:27 +0200

    subida ejercicios del 7 al 10, el 10 sin acabar

---
+++ Práctica 3/ejercicio_7.ps1
@@ -0,0 +1,15 @@
+﻿<#
+Ejercicio 7. Escribir un programa que almacene la cadena de caracteres contraseña en una
+variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña
+introducida por el usuario coincide con la guardada en la variable sin tener en cuenta
+mayúsculas y minúsculas.
+#>
+
+$contraseña = "tortillapatata"
+$contraseña_user = Read-Host "Introduzca la contraseña"
+
+if($contraseña_user -ieq $contraseña){
+    Write-Host "Contraseña correcta"
+}else{
+    Write-Host "Contraseña incorrecta"
+}
---
+++ Práctica 3/ejercicio_8.ps1
@@ -0,0 +1,17 @@
+﻿<#
+Ejercicio 8. Los alumnos de un curso se han dividido en dos grupos A y B de acuerdo al sexo
+y el nombre. El grupo A esta formado por las mujeres con un nombre anterior a la M y los
+hombres con un nombre posterior a la N y el grupo B por el resto. Escribir un programa que
+pregunte al usuario su nombre y sexo, y muestre por pantalla el grupo que le corresponde.
+#>
+
+$nombre = Read-Host "Introduce tu nombre"
+$sexo = Read-Host "¿Eres hombre o mujer?"
+
+ if ($sexo -ieq "mujer" -and $nombre -le "M*"){
+    Write-Host "Perteneces al grupo A."
+ }elseif ($sexo -ieq "hombre" -and $nombre -ge "N*"){
+    Write-Host "Perteneces al grupo A."
+ }else{
+    Write-Host "Perteneces al grupo B."
+ }
