18afd92d492077dc5734e7ecff6dcaa5b0f85665
alufer8673 <alufer8673@ieselcaminas.org>
2023-09-25 09:52:26 +0200

    Práctica 4 hasta ejercicio 2

---
+++ Práctica 4/ejercicio_1.ps1
@@ -0,0 +1,13 @@
+﻿<#
+Ejercicio1. Escribir un programa que pregunte el nombre del usuario en la consola y un
+número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces
+como el número introducido.
+#>
+
+$nombre = Read-Host "Introduce tu nombre"
+[int] $num = Read-Host "Introduce un número entero"
+
+while($num -gt 0){
+    Write-Host "$nombre"
+    $num = $num - 1
+}
---
+++ Práctica 4/ejercicio_2.ps1
@@ -0,0 +1,10 @@
+﻿<#
+Ejercicio 2. Escribir un programa que pregunte al usuario su edad y muestre por pantalla
+todos los años que ha cumplido (desde 1 hasta su edad).
+#>
+
+[int] $edad = Read-Host "Introduce tu edad"
+
+for ($a = 1; $a -le $edad; $a++){
+    Write-Host "$a"
+}
