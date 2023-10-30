04ce67408123d2b64ec7a2d876781586a4968f11
alufer8673 <alufer8673@ieselcaminas.org>
2023-10-30 08:42:27 +0100

    2023_10_30

---
+++ UT2_Administracion_servicio_de_directorio_en_Windows/UT2. Práctica 7.ps1
@@ -0,0 +1,7 @@
+﻿$empleados = Import-Csv 'C:\Users\Administrador\Desktop\archivos\empleados.csv' -Delimiter ";"
+
+foreach ($emp in $empleados){
+    if ($($emp.departamento) -ieq "personal"){
+        Set-ADUser -Identity "$($emp.nombre)$($emp.apellido)" -ProfilePath \\Empresa-dc1\Empresa_users$\"$($emp.nombre)$($emp.apellido)"   
+    }
+}
