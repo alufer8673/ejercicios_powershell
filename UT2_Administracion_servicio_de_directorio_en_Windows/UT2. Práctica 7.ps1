3e4c7723335ae40abf2a5625bc2ec8887d6e151e
alufer8673 <alufer8673@ieselcaminas.org>
2023-10-30 09:24:35 +0100

    2023_10_30

--- UT2_Administracion_servicio_de_directorio_en_Windows/UT2. Práctica 7.ps1
+++ UT2_Administracion_servicio_de_directorio_en_Windows/UT2. Práctica 7.ps1
@@ -2,6 +2,6 @@
 
 foreach ($emp in $empleados){
     if ($($emp.departamento) -ieq "personal"){
-        Set-ADUser -Identity "$($emp.nombre)$($emp.apellido)" -ProfilePath \\Empresa-dc1\Empresa_users$\"$($emp.nombre)$($emp.apellido)"   
+        Set-ADUser -Identity "$($emp.nombre)$($emp.apellido)" -ProfilePath "\\Empresa-dc1\Empresa_users$\$($emp.nombre)$($emp.apellido)"   
     }
 }
